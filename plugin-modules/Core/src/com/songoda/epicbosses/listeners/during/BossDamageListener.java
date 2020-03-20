8e910166744f529afc5dc331c933698d7b346ebf
Brianna <bri@songoda.com>
2020-03-19 20:07:22 -0400

    Lets remove active holders on death.

--- plugin-modules/Core/src/com/songoda/epicbosses/listeners/during/BossDamageListener.java
+++ plugin-modules/Core/src/com/songoda/epicbosses/listeners/during/BossDamageListener.java
@@ -13,9 +13,7 @@ import org.bukkit.entity.*;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
-import org.bukkit.event.entity.EntityCombustEvent;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
-import org.bukkit.event.entity.EntityExplodeEvent;
 import org.bukkit.event.entity.ExplosionPrimeEvent;
 
 /**
